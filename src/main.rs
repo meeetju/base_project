10c782c4dcad9e02c7730e77bc3ac882764021c3
mm <meeetju@gmail.com>
2023-11-30 22:41:32 +0100

    Works on project

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,24 @@
-fn main() {
-    println!("Hello, world!");
+use base_project::configuration::get_configuration;
+use base_project::startup::run;
+use base_project::telemetry::{get_subscriber, init_subscriber};
+use secrecy::ExposeSecret;
+use sqlx::PgPool;
+use std::net::TcpListener;
+
+#[tokio::main]
+async fn main() -> Result<(), std::io::Error> {
+    let subscriber = get_subscriber("base_project".into(), "info".into(), std::io::stdout);
+    init_subscriber(subscriber);
+
+    let configuration = get_configuration().expect("Unable to read configuration file");
+    let db_connection_pool =
+        PgPool::connect_lazy(&configuration.database.connection_string().expose_secret())
+            .expect("Failed to create Postgres connection pool.");
+    let address = format!("127.0.0.1:{}", configuration.application_port);
+    let listener = TcpListener::bind(address)?;
+
+    let port_picked = listener.local_addr().unwrap().port();
+    dbg!(format!("Listen on port {port_picked}"));
+
+    run(listener, db_connection_pool)?.await
 }
---
+++ src/routes/subscriptions.rs
@@ -0,0 +1,55 @@
+use actix_web::{web, HttpResponse};
+use chrono::Utc;
+use sqlx::PgPool;
+use uuid::Uuid;
+
+#[derive(serde::Deserialize)]
+pub struct FormData {
+    email: String,
+    name: String,
+}
+
+#[tracing::instrument(
+    name = "Adding a new subscriber",
+    skip(form, db_connection_pool),
+    fields(
+        subscriber_email = %form.email,
+        subscriber_name = %form.name
+    )
+)]
+pub async fn subscribe(
+    form: web::Form<FormData>,
+    db_connection_pool: web::Data<PgPool>,
+) -> HttpResponse {
+    match insert_subscriber(&form, &db_connection_pool).await {
+        Ok(_) => HttpResponse::Ok().finish(),
+        Err(_) => HttpResponse::InternalServerError().finish(),
+    }
+}
+
+#[tracing::instrument(
+    name = "Saving new subscriber details in the database",
+    skip(form, db_connection_pool)
+)]
+pub async fn insert_subscriber(
+    form: &FormData,
+    db_connection_pool: &PgPool,
+) -> Result<(), sqlx::Error> {
+    sqlx::query!(
+        r#"
+        INSERT INTO subscriptions (id, email, name, subscribed_at)
+        VALUES ($1, $2, $3, $4)
+        "#,
+        Uuid::new_v4(),
+        form.email,
+        form.name,
+        Utc::now()
+    )
+    .execute(db_connection_pool)
+    .await
+    .map_err(|e| {
+        tracing::error!("Failed to execute query: {:?}", e);
+        e
+    })?;
+    Ok(())
+}
